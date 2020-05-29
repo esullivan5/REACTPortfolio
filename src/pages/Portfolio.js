4f0544eeea877d51ef93da94f12d445014dd8940
Ginseng <esullivann@hotmail.com>
2020-05-28 20:55:10 -0600

    updated photos and links in portfolio

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -27,36 +27,36 @@ function Portfolio() {
             <Row>
               <Project
                 name="Progressive Budget App"
-                imgPath={require("../components/assets/images/note-taker.png")}
+                imgPath={require("../components/assets/images/BudgetTracker.png")}
                 linkURL="https://treat-yoselff.herokuapp.com"
               />
             </Row>
             <Row>
               <Project
-                name="Code Quiz"
-                imgPath={require("../components/assets/images/weatherapp.jpg")}
-                linkURL="https://esullivan5.github.io/CodeQuiz/"
-              />
-            </Row>
-            <Row>
-              <Project
                 name="Lyric.Me"
-                imgPath={require("../components/assets/images/weatherapp.jpg")}
+                imgPath={require("../components/assets/images/LyricMe.png")}
                 linkURL="https://sk2060sk2060.github.io/group-project-1/"
               />
             </Row> 
             <Row>
               <Project
                 name="STOCK-er"
-                imgPath={require("../components/assets/images/weatherapp.jpg")}
+                imgPath={require("../components/assets/images/Stock-ErApp.png")}
                 linkURL="https://alexngn2.github.io/class-porject-2//"
               />
             </Row>
             <Row>
               <Project
                 name="Be Green"
-                imgPath={require("../components/assets/images/weatherapp.jpg")}
-                linkURL="#"
+                imgPath={require("../components/assets/images/beGreen.png")}
+                linkURL="https://github.com/sk2060sk2060/be-green"
+              />
+            </Row>
+            <Row>
+              <Project
+                name="Fitness Tracjer"
+                imgPath={require("../components/assets/images/FitnessTracker.png")}
+                linkURL="https://befitordietrying.herokuapp.com/?id=5ed05594625ffc0017825614"
               />
             </Row>
           </div>
