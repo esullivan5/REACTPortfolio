f4f4f3c32de94d5545c9082c7798711e6da1d553
Ginseng <esullivann@hotmail.com>
2020-05-28 20:09:41 -0600

    Updates

---
+++ precache-manifest.8d0b9e7d9b78ab1cf905b2ebf73ac640.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7656124689a4b3a478f54eeb335f22b",
+    "url": "/REACTPortfolio/index.html"
+  },
+  {
+    "revision": "36fb7075c1651219aa4c",
+    "url": "/REACTPortfolio/static/css/main.afe206d6.chunk.css"
+  },
+  {
+    "revision": "e9baa1a238768ec076f5",
+    "url": "/REACTPortfolio/static/js/2.b1233707.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/REACTPortfolio/static/js/2.b1233707.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36fb7075c1651219aa4c",
+    "url": "/REACTPortfolio/static/js/main.885a937b.chunk.js"
+  },
+  {
+    "revision": "5e1eb6e969e9b24f9d86",
+    "url": "/REACTPortfolio/static/js/runtime-main.16b62946.js"
+  },
+  {
+    "revision": "ec90ab1cb518bdb0c64c94ff28eac8ff",
+    "url": "/REACTPortfolio/static/media/background-image.ec90ab1c.png"
+  },
+  {
+    "revision": "99f64c15dbabd2a47a636ba172ce0d6d",
+    "url": "/REACTPortfolio/static/media/me.png.99f64c15.jpg"
+  }
+]);
